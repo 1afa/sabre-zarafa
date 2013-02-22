7ba51606a1812ecf62cb013423aaf9847f7b00ae
Alfred Klomp <alfred@bokxing-it.nl>
2013-02-22 11:59:27 +0100

    Project page URL: change from Google Code to GitHub
    This project's repository is at GitHub, not at Google Code.
    Change the header comments to reflect this.

--- vcard/VCardParser.php
+++ vcard/VCardParser.php
@@ -20,7 +20,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Project page: <http://code.google.com/p/sabre-zarafa/>
+ * Project page: <http://github.com/bokxing-it/sabre-zarafa/>
  * 
  */
  
