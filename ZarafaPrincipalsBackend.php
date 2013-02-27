beb18b34e97a032d1e033c13f2b1ea2321ece1b2
Alfred Klomp <alfred@bokxing-it.nl>
2013-02-27 12:36:28 +0100

    Add copyright string to files we touched

--- ZarafaPrincipalsBackend.php
+++ ZarafaPrincipalsBackend.php
@@ -1,6 +1,7 @@
 <?php
 /*
  * Copyright 2011 - 2012 Guillaume Lapierre
+ * Copyright 2012 - 2013 Bokxing IT, http://www.bokxing-it.nl
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License, version 3, 
