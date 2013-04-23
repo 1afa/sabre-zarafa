53b54d8d8bf94e8737ca199ff734057af54fc7de
Alfred Klomp <alfred@bokxing-it.nl>
2013-04-23 14:42:36 +0200

    Don't redefine FALSE() if already defined
    Necessary when you want to include Sabre-Zarafa in another project that already
    defines FALSE(). The author encountered this.

--- common.inc.php
+++ common.inc.php
@@ -42,7 +42,10 @@
 	define ('PR_CARDDAV_AB_CONTACT_COUNT',			mapi_prop_tag(PT_LONG,    CARDDAV_CUSTOM_PROPERTY_ID | 0x0003));
 	define ('PR_CARDDAV_RAW_DATA_VERSION',			mapi_prop_tag(PT_STRING8, CARDDAV_CUSTOM_PROPERTY_ID | 0x0004));
 
+if (!function_exists('FALSE'))
+{
 	function FALSE ($expr)
 	{
 		return ($expr === FALSE);
 	}
+}
